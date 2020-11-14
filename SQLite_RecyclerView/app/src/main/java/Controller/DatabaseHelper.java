34ae3a58a6121957927f0621d9cf0fe95cfb1056
osama naji <osama99654@gmail.com>
2020-11-14 16:47:55 +0300

    Add InserdData method and UpdateData method and DeleteData metod in DataBaseHelper

--- SQLite_RecyclerView/app/src/main/java/Controller/DatabaseHelper.java
+++ SQLite_RecyclerView/app/src/main/java/Controller/DatabaseHelper.java
@@ -1,11 +1,13 @@
 package Controller;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import Model.Data;
 import Util.Utils;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -27,6 +29,37 @@ db.execSQL("CREATE TABLE "+Utils.TABLE_NAME + " (" + Utils.COLOUMN_ID +" INTEGER
 
         db.execSQL("DROP TABLE IF EXISTS "+ Utils.TABLE_NAME);
         onCreate(db);
+
+    }
+
+    public long InsertData (Data data){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues values =new ContentValues();
+        values.put(Utils.COLOUMN_NAME,data.getName());
+        values.put(Utils.COLOUMN_LNAME,data.getLname());
+        values.put(Utils.COLOUMN_AGE,data.getAge());
+        values.put(Utils.COLOUMN_DESCRIPTION,data.getDesrption());
+        Long id =database.insert(Utils.TABLE_NAME,null,values);
+        database.close();
+        return id;
+
+    }
+    public int UpdateData (Data data){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues values =new ContentValues();
+        values.put(Utils.COLOUMN_NAME,data.getName());
+        values.put(Utils.COLOUMN_LNAME,data.getLname());
+        values.put(Utils.COLOUMN_AGE,data.getAge());
+        values.put(Utils.COLOUMN_DESCRIPTION,data.getDesrption());
+        int id =database.update(Utils.TABLE_NAME,values,Utils.COLOUMN_ID +"=?",new String[]{Utils.COLOUMN_ID});
+database.close();
+        return id;
+    }
+
+    public void DeleteData (Data data){
+        SQLiteDatabase database = this.getWritableDatabase();
+        database.delete(Utils.TABLE_NAME,Utils.COLOUMN_ID +"=?",new String[]{Utils.COLOUMN_ID});
+        database.close();
 
     }
 }
