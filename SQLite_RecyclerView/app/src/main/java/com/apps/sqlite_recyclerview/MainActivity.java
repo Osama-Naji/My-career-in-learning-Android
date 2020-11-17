d6bd1511b04fdae5c976c0246796bd4b546ca97f
osama naji <osama99654@gmail.com>
2020-11-17 15:04:27 +0300

    Completed MainActivity class (Project)

--- SQLite_RecyclerView/app/src/main/java/com/apps/sqlite_recyclerview/MainActivity.java
+++ SQLite_RecyclerView/app/src/main/java/com/apps/sqlite_recyclerview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.apps.sqlite_recyclerview;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,13 +8,26 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
+import Controller.Adapter;
+import Controller.DatabaseHelper;
+import Views.AddData;
+
 public class MainActivity extends AppCompatActivity {
+
+
+   public static Adapter adapter;
+    DatabaseHelper helper ;
+    RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +36,40 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        recyclerView = findViewById(R.id.RecyclearViewID);
+helper = new DatabaseHelper(this);
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                Intent intent = new Intent( MainActivity.this,AddData.class);
+
+                startActivity(intent);
+
+                finish();
+
             }
         });
+
+        adapter = new Adapter(this,helper.getAllData(),helper);
+        RecyclerView.LayoutManager layoutManager= new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
     }
+
+    public static void notifyAdapter(){
+        adapter.notifyDataSetChanged();
+
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
